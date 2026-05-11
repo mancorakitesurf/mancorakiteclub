dcedefdd6d8a693601e43335f676f203862fb34a
MatiasCarmen <U22209935@utp.edu.pe>
2026-05-10 22:14:26 -0500

    fix: change i18nContext.js to i18nContext.jsx in Hero.jsx to fix vite import error

--- client/src/sections/home/Hero.jsx
+++ client/src/sections/home/Hero.jsx
@@ -1,7 +1,7 @@
 import { FaWhatsapp } from 'react-icons/fa'
 import FullscreenHero from '../../components/ui/FullscreenHero.jsx'
 import { buildWhatsAppUrl, defaultInquiryMessage } from '../../lib/whatsapp.js'
-import { useI18n } from '../../app/providers/i18nContext.js'
+import { useI18n } from '../../app/providers/i18nContext.jsx'
 import { HOME_HERO_SLIDES } from './homeContent.js'
 
 function Hero() {
